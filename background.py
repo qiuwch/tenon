15e7cac22d68a4e5881b636477e2ac311d0f8695
Weichao Qiu <qiuwch@gmail.com>
2015-07-24 01:37:08 +0800

    Add BG support.

--- background.py
+++ background.py
@@ -2,6 +2,17 @@
 '''
 
 import bpy
+import glob
+
+INRIA_DIR = '/Users/qiuwch/Dropbox/Workspace/CG/rendering/background/INRIA/'
 
 def setBackground(filename):
-	pass
+	realpath = bpy.path.abspath(filename)
+	img = bpy.data.images.load(realpath); 
+	# bpy.data.images['Beautiful-Garden-HD-Wallpapers.jpg'];
+	bpy.data.textures['bg'].image = img;
+
+
+def INRIA_BG(id):
+	files = glob.glob(INRIA_DIR + '*.jpg')
+	setBackground(files[id])
