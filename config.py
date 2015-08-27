c664994f235ebf3964346ee1eaefeb049fcaf84a
Weichao Qiu <qiuwch@gmail.com>
2015-08-27 17:42:34 +0800

    Rename config file.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Include the directory of this script, to make it usable as a package.
 import sys
 import imp
-PWD = '/Users/qiuwch/Dropbox/Workspace/research/tenon'
+PWD = '/Users/qiuwch/Dropbox/Workspace/research/'
 INRIA_DIR = '/q/data/INRIA/'
 RENDER_OUTPUT_DIR = '/q/cache/render_output/'
 sys.path.append(PWD)
