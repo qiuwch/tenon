0ff41c232291ce85c1e35e1c75d9186d17d1ba04
Weichao Qiu <qiuwch@gmail.com>
2015-08-27 16:33:21 +0800

    Update init script.

--- init.py
+++ init.py
@@ -1,30 +1,5 @@
-# filename = '/Users/qiuwch/Dropbox/Workspace/CG/rendering/tenon/init.py'
-# exec(compile(open(filename).read(), filename, 'exec'))
-
+# Include the directory of this script, to make it usable as a package.
 import sys
 sys.path.append('/Users/qiuwch/Dropbox/Workspace/CG/rendering/tenon/')
-# import camera
-# import skeleton
-# import background
-# import animate
-# import _render
-# import bpy
-# import imp
-# import tenon
-
-# if "camera" in locals():
-# 	import imp
-# 	imp.reload(camera)
-# 	imp.reload(skeleton)
-# 	imp.reload(background)
-# 	imp.reload(animate)
-# 	imp.reload(_render)
-# else:
-# 	import camera
-# 	import skeleton
-# 	import background
-# 	import animate
-# 	import _render
-# 	import bpy
 
 
