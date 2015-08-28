c1379d26129e61e44adb0c31b2e0cc9a54f5ae1e
Weichao Qiu <qiuwch@gmail.com>
2015-08-28 16:34:01 +0800

    Update demo scripts.

--- demo.py
+++ demo.py
@@ -15,31 +15,40 @@ def realisticMode():
 
 def batchRender(num):
 	''' Render number of frames '''
+	infoFilename = render.outputFolder + '/info.csv'
+	fInfo = open(infoFilename, 'w')
 	# TODO: make verbose output fresh during execution?
-	len = min(num, bpy.context.scene.frame_end)
+	nImg = min(num, bpy.context.scene.frame_end)
 
 	bid = 1 # bid is background id, should be randomly chosen
 	print('Generating image')
-	for fid in range(len):
+
+	fInfo.write('imageId, finImgame, frameId, background\n')
+	seq = range(0, 200, 5)
+	seq = seq[0:nImg]
+	for ii in range(len(seq)):
+		fid = seq[ii]
 		# fid is frame id
 		# setCamPos(0)
 		tenon.background.setINRIA(bid)
 
 		tenon.animate.toFrame(fid); 
-		prefix = 'f%d_b%d' % (fid, bid)
+		# prefix = 'f%d_b%d' % (fid, bid)
+		prefix = '%04d' % ii
+		fInfo.write('%04d, %s, %d, %d' % (ii, prefix, fid, bid))
+
 		# prefix = str(prefix)
-		# render current frames to image
-		# output to files
 
 		render.realisticMode()
-		filename = prefix + '-real.png'
-		render.write(filename)
-		print(bpy.path.abspath(filename))
+		render.write('/imgs/' + prefix + '.png')
 
 		render.boundaryMode()
-		render.write(prefix + '-edge.png')
+		render.write('/mask/' + prefix + '.png')
+		
 		render.jointsMode()
-		render.write(prefix + '-joint.png')
+		render.write('/skel/' + prefix + '.png')
+
+	fInfo.close()
 
 
 def setupSkeleton():
