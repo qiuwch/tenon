3ef0e8bd1df71db5d8858ea8b978475b7fb8d456
Weichao Qiu <qiuwch@gmail.com>
2015-09-10 21:11:08 +0800

    Minor update.

--- util/plot_keypoint.m
+++ util/plot_keypoint.m
@@ -1,6 +1,6 @@
-im = imread('/q/cache/render_output/imgs/0000.png');
+im = imread('/q/cache/render_output/imgs/im0001.png');
 imshow(im);
-A = csvread('/q/cache/render_output/joint.csv');
+A = csvread('/q/cache/render_output/joint-PC-MATLAB.csv');
 X = A(:,1:2:27);
 Y = A(:,2:2:28);
 hold on;
