b773ca9098d962a604ac7ac6619756e97bc8ce95
Weichao Qiu <qiuwch@gmail.com>
2015-09-10 17:58:35 +0800

    Major update to crop script. It is able to handle annotation now.

---
+++ util/crop_img.m
@@ -0,0 +1,37 @@
+function [crop, U, V] = crop_img(im, depth, X, Y)
+	% crop : cropped image
+	% U : mapped X coordinate
+	% V : mapped Y coordinate
+
+	fg = depth(:,:,1) > 0;
+
+	% compute the bounding box of fg
+	imSize = size(depth(:,:,1));
+	[fgY, fgX] = ind2sub(imSize, find(fg)); % This is different from X, Y
+
+	h = max(fgY) - min(fgY) + 1; w = max(fgX) - min(fgX) + 1;
+
+	marginRatio = 0.1;
+	cmargin = max(h, w) * marginRatio;
+
+	ch = h + 2 * cmargin; % h of crop patch
+	cw = w + 2 * cmargin;
+
+	s = 150.0 / max(ch, cw);
+	ch = s * ch; cw = s * cw;
+	tx = - s * (min(fgX) - cmargin) + 1; 
+	% Translation in x, map (min(X) - margin) to 1, what will happen in it is smaller than 1
+	ty = - s * (min(fgY) - cmargin) + 1; % translation in y
+
+	A = [s  , 0  , 0; % Translate than scale
+		 0  , s  , 0;
+		 tx , ty , 1];
+
+
+	T = maketform('affine', A);
+	XData = [1, cw]; YData = [1, ch];
+	crop = imtransform(im, T, 'XData', XData, 'YData', YData);
+
+	% Mapping of landmark locations
+	[U, V] = tformfwd(T, X, Y);
+end
--- util/crop_imgs.m
+++ util/crop_imgs.m
@@ -1,65 +1,36 @@
 function crop_imgs()
     % Crop the image, to make it suitable for LSP format
-    clear all;
-    origDir = '/q/cache/render_output/9.7_v1/';
-    disp(origDir);
+    origDir = '/q/cache/render_output/';
+    cropDir = [origDir 'crop/'];
+    anno = csvread([origDir 'joint-PC-MATLAB.csv']);
+    joints = zeros(2, size(anno,2)/2, size(anno,1));
 
-    num = 40;
+    for fileid = 0:(size(anno, 1)-1)
+        % Get annotation
+        X = anno(fileid+1, 1:2:end);
+        Y = anno(fileid+1, 2:2:end);
 
-    for i = 0:(num-1)
         % Get mask
-        depth_fname = sprintf([origDir 'depth/%04d.png'], i);
+        depth_fname = sprintf([origDir 'depth/%04d.png'], fileid);
         depth = imread(depth_fname);
-        fg = depth(:,:,1) > 0;
-
-        % compute the bounding box of fg
-        imSize = size(depth(:,:,1));
-        [Y, X] = ind2sub(imSize, find(fg));
-        fgBBmask = false(imSize);
-        fgBBmask(min(Y):max(Y), min(X):max(X)) = 1;
-        bb = [min(Y), max(Y), min(X), max(X)];
-
-    %     fgIdx = fg;
-        fgIdx = fgBBmask;
 
         % for type = {'imgs', 'depth', 'skel'}
         for type = {'imgs'}
             type = type{1};
-            imfname = sprintf([origDir type '/%04d.png'], i);
+            imfname = sprintf([origDir type '/%04d.png'], fileid);
             im = imread(imfname);
             im = im2double(im);
-%             crop = do_crop(im, fgIdx);
-            crop = bb_crop(im, bb);
-            crop_imfname = sprintf([origDir 'crop/' type '/%04d.png'], i);
+            [crop, U, V] = crop_img(im, depth, X, Y);
+
+            % Save cropped image
+            crop_imfname = sprintf([cropDir type '/%04d.png'], fileid);
             imwrite(crop, crop_imfname);
+
+            % Save annotation
+            joints(1, :, fileid+1) = U;
+            joints(2, :, fileid+1) = V;
         end
     end
+    save([cropDir 'joints.mat'], 'joints');
 end
 
-function crop = bb_crop(im, bb)
-    h = bb(2) - bb(1); w = bb(4) - bb(3);
-    h = max(h, w); w = max(h, w);
-    imh = size(im, 1); imw = size(im, 2);
-    
-    ratio = 0.1;
-    t = max(uint32(bb(1) - ratio * h), 1);
-    b = min(uint32(bb(2) + ratio * h), imh);
-    l = max(uint32(bb(3) - ratio * w), 1);
-    r = min(uint32(bb(4) + ratio * w), imw);
-    crop = im(t:b, l:r, :);
-    sz = size(crop(:,:,1));
-    if sz(1) > sz(2) % height > width
-        crop = imresize(crop, 150.0 / sz(1)); % no interporlation for joint labeling
-    else
-        crop = imresize(crop, 150.0 / sz(2));
-    end
-end
-
-function crop = do_crop(im, fgIdx)
-    crop = zeros(size(im));
-    for ci = 1:3 % channel index
-        c = im(:,:,ci);
-        c(~fgIdx) = 0;
-        crop(:,:,ci) = c;
-    end
-end
