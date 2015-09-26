67d56259f6116c3ebc028aa3546a764d22c6e51b
Weichao Qiu <qiuwch@gmail.com>
2015-09-26 10:08:26 -0700

    Add python site package folder, import more packages for convenience.

--- init.py
+++ init.py
@@ -4,8 +4,19 @@ import imp
 
 PWD = '/Users/qiuwch/Dropbox/Workspace/research/CG/'
 sys.path.append(PWD)
+
+# Add python site packages into blender, so that I can use third party libs.
+# TODO: check python version of blender first
+sys.path.append('/usr/local/lib/python3.4/site-packages/')
+print(sys.version) # This is the python version. Do not mix python2 and 3 libs.
+
 def r(v):
     imp.reload(v)
 
-import tenon.demo 
-
+# Import for the convinience of interactive shell
+import tenon.demo as td
+from tenon.render import render
+from tenon.config import *
+import tenon.pose
+import tenon.export
+from tenon.bpyutil import *
