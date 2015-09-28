6142641eb349bb838f422ba2f101baa06ebac6f3
Weichao Qiu <qiuwch@gmail.com>
2015-09-28 15:17:05 -0700

    Add code for control point inverse kinematics.

--- pose.py
+++ pose.py
@@ -3,61 +3,102 @@ import bpy
 import mathutils
 import math
 def rest():
-	""" Set the human body to rest pose """
+    """ Set the human body to rest pose """
 
-	# scn.objects.active = rig
-	# TODO: Bug fix!! This code can not work with locked property
-	# bpy.ops.object.mode_set(mode='POSE')
-	# bpy.ops.pose.select_all(action='SELECT')
-	# bpy.ops.pose.rot_clear()
-	# bpy.ops.pose.loc_clear()
-	# bpy.ops.pose.scale_clear()
+    # scn.objects.active = rig
+    # TODO: Bug fix!! This code can not work with locked property
+    # bpy.ops.object.mode_set(mode='POSE')
+    # bpy.ops.pose.select_all(action='SELECT')
+    # bpy.ops.pose.rot_clear()
+    # bpy.ops.pose.loc_clear()
+    # bpy.ops.pose.scale_clear()
 
-	obj = bpy.data.objects['human_model']
-	for bone in obj.pose.bones:
-		bone.matrix_basis = mathutils.Matrix() # Set it to identity matrix
+    obj = bpy.data.objects['human_model']
+    for bone in obj.pose.bones:
+        bone.matrix_basis = mathutils.Matrix() # Set it to identity matrix
 
 def TPose():
-	''' This is not working at this moment '''
-	version = 'v3'
-	print(version)
+    ''' This is not working at this moment '''
+    version = 'v3'
+    print(version)
 
-	obj = bpy.data.objects['human_model']
-	for poseBone in obj.pose.bones:
-		poseBone.matrix_basis = mathutils.Matrix() # Set it to identity matrix
-		# poseBone.bone.matrix_local = mathutils.Matrix() # this is very wrong
+    obj = bpy.data.objects['human_model']
+    for poseBone in obj.pose.bones:
+        poseBone.matrix_basis = mathutils.Matrix() # Set it to identity matrix
+        # poseBone.bone.matrix_local = mathutils.Matrix() # this is very wrong
 
-		old = poseBone.bone.matrix_local
+        old = poseBone.bone.matrix_local
 
-		loc, rot, sca = old.decompose()
+        loc, rot, sca = old.decompose()
 
-		transMat = mathutils.Matrix.Translation(loc)
-		scaleMat = mathutils.Matrix.Scale(sca[0], 4, (1, 0, 0)) * mathutils.Matrix.Scale(sca[1], 4, (0, 1, 0)) * mathutils.Matrix.Scale(sca[2], 4, (0, 0, 1))
+        transMat = mathutils.Matrix.Translation(loc)
+        scaleMat = mathutils.Matrix.Scale(sca[0], 4, (1, 0, 0)) * mathutils.Matrix.Scale(sca[1], 4, (0, 1, 0)) * mathutils.Matrix.Scale(sca[2], 4, (0, 0, 1))
 
-		# rotMat = mathutils.Matrix.Rotation(rot)
+        # rotMat = mathutils.Matrix.Rotation(rot)
 
-		# poseBone.bone.matrix_local = transMat # * rotMat * scaleMat
-		# Without scale is weird
-		poseBone.bone.matrix_local = transMat * scaleMat
+        # poseBone.bone.matrix_local = transMat # * rotMat * scaleMat
+        # Without scale is weird
+        poseBone.bone.matrix_local = transMat * scaleMat
 
 
 def testPose():
-	''' Only change the forearem '''
-	human = bpy.data.objects['human_model']
+    ''' Only change the forearem '''
+    human = bpy.data.objects['human_model']
 
-	forearm = human.pose.bones['forearm.fk.L']
-	upper_arm = human.pose.bones['upper_arm.fk.L']
+    forearm = human.pose.bones['forearm.fk.L']
+    upper_arm = human.pose.bones['upper_arm.fk.L']
 
-	def setBoneYZXEuler(bone, x, y, z):
-		print(bone.rotation_mode)
-		assert(bone.rotation_mode == 'YZX')
+    def setBoneYZXEuler(bone, x, y, z):
+        print(bone.rotation_mode)
+        assert(bone.rotation_mode == 'YZX')
 
-		# Just some random number for test
-		bone.rotation_euler.x = x / 180 * math.pi # This is radius
-		bone.rotation_euler.y = y / 180 * math.pi
-		bone.rotation_euler.z = z / 180 * math.pi
+        # Just some random number for test
+        bone.rotation_euler.x = x / 180 * math.pi # This is radius
+        bone.rotation_euler.y = y / 180 * math.pi
+        bone.rotation_euler.z = z / 180 * math.pi
 
-	setBoneYZXEuler(forearm, 90, 0, 0)
-	setBoneYZXEuler(upper_arm, 43, 54, 65)
+    setBoneYZXEuler(forearm, 90, 0, 0)
+    setBoneYZXEuler(upper_arm, 43, 54, 65)
 
+def createControlPoint():
+    names = [
+        'ankle.r',
+        'knee.r',
+        'hip.r',
+        'hip.l',
+        'knee.l',
+        'ankle.l',
+        'wrist.r',
+        'elbow.r',
+        'shoulder.r',
+        'shoulder.l',
+        'elbow.l',
+        'wrist.l',
+        'neck',
+        'headTop'
+    ]
+    for jointName in names:
+        controlEmpty = bpy.data.objects.get(jointName)
+        if controlEmpty == None:
+            bpy.ops.object.empty_add()
+            controlEmpty = bpy.context.object
+            controlEmpty.name = jointName
 
+    # setup joint constraint manually
+
+def animateCP():
+    # Load data from exported csv file
+    import pandas as pd
+    pts = pd.read_csv('/q/cache/lsp_2d_3d/0001.csv')
+    #
+    for i in range(len(pts.x)):
+        jointName = '%04d' % i
+        controlEmpty = bpy.data.objects.get(jointName)
+        if controlEmpty == None:
+            bpy.ops.object.empty_add()
+            controlEmpty = bpy.context.object
+            controlEmpty.name = jointName
+        # 
+        controlEmpty.location.x = pts.x[i]
+        controlEmpty.location.y = pts.y[i]
+        controlEmpty.location.z = pts.z[i]
