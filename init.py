a2da2af49ab9d5f37f5af31aedbdbafa7d25bbfb
Weichao Qiu <qiuwch@gmail.com>
2015-09-28 20:12:01 -0700

    Clean the init script, make it cross platform.

--- init.py
+++ init.py
@@ -1,25 +1,28 @@
 # Script to load tenon into blender
-import sys
-import imp
+import sys, imp, re, os
 
-PWD = '/Users/qiuwch/Dropbox/Workspace/research/CG/'
-sys.path.append(PWD)
+PWD='/Users/qiuwch/Dropbox/Workspace/research/CG/'
+sitePkg='/usr/local/lib/python3.4/site-packages/'
+
+for v in [PWD, sitePkg]:
+	sys.path.append(v)
 
 # Add python site packages into blender, so that I can use third party libs.
 # TODO: check python version of blender first
-sys.path.append('/usr/local/lib/python3.4/site-packages/')
 print(sys.version) # This is the python version. Do not mix python2 and 3 libs.
 
 def r(v):
     imp.reload(v)
 
-# Import for the convinience of interactive shell
-import tenon.demo as td
-from tenon.render import render
-from tenon.config import *
-import tenon.pose
-import tenon.export
-from tenon.bpyutil import *
-import tenon.task
-
-# TODO: Clean this area
+if __name__ == '__main__':
+	with open('./init.py') as f:
+		data = f.read()
+		re.sub('PWD = .*', 'PWD = %s' % os.path.abspath('.'), data)
+		print data
+else:
+	# Import for the convinience of interactive shell
+	import tenon.demo as td
+	from tenon.render import render
+	from tenon.config import *
+	from tenon.bpyutil import *
+	import tenon.pose, tenon.export, tenon.task
