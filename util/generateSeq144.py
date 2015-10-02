fe2cc65a1ef28362d36f63ea7bc294e852f4caa7
Weichao Qiu <qiuwch@gmail.com>
2015-10-01 20:55:15 -0700

    Clean job generation code.

--- util/generateSeq144.py
+++ util/generateSeq144.py
@@ -1,66 +1,44 @@
 from datetime import datetime
-import glob
 import random
 
 import sys
 sys.path.append('..')
-from task import Task
+from task import Task, Job
+import config
 
-def pathHelper(relpath):
-	path = '/Users/qiuwch/Dropbox/Workspace/research/CG/code/tenon/scenes' + relpath
-	# path = '..' + relpath
-	return path
-
-def getClothList(): # TODO combine this with task execution
-    jpgs = glob.glob(pathHelper('//textures/*.jpg'))
-    pngs = glob.glob(pathHelper('//textures/*.png'))
-    files = jpgs + pngs
-    return files
-
-def getBGList():
-    BGlist = glob.glob(pathHelper('//background/INRIA/*.jpg'))
-    return BGlist
 
 def generateSeq144():
     ''' Generate BVH Sequence 144_43, All types of clothes, Randomly pick a background '''
 
-    # Load cloth list
-    clothes = getClothList()
+    clothes = config.getClothList()
     nCloth = len(clothes)
-    print 'Number of clothes: %d' % nCloth
-
-    # Load background list
-    bgs = getBGList()
+    bgs = config.getBGList()
     nBG = len(bgs)
-    print 'Number of background: %d' % nBG
+    print 'Number of clothes: %d, background: %d' % (nCloth, nBG)
 
     # Number of frames defined in the bvh file
     nFrame = 935
     print 'Number of poses: %d' % nFrame
 
     now = datetime.now()
-    f = open(pathHelper('//%02d.%02d_Seq144.csv' % (now.month, now.day)), 'w')
-    f.write(Task.header() + '\n')
 
-    count = 0
-    # for ic in range(nCloth):
+    tasks = []
+
     for iframe in range(nFrame):
         task = Task()
-        task.rowId = count
-        # task.clothId = ic
         task.clothId = random.randrange(0, nCloth)
         task.frameId = iframe
         task.backgroundId = random.randrange(0, nBG) # Pick from uniform
         task.mode = 'ipjd' # See definition
-        line = task.serilizeToLine()
+        tasks.append(task)
 
-        f.write(line + '\n')
-        count += 1
-
-    f.close()
-
-def generateTask():
-    generateSeq144()
+    j = Job()
+    j.name = 'ClothParsing'
+    j.date = '%02d.%02d' % (now.month, now.day)
+    j.comment = 'This is the first batch task definition'
+    j.outputFolder = '/q/cache/clothParsing'
+    j.tasks = tasks
+    j.save(config.pathHelper('//%02d.%02d_Seq144.csv' % (now.month, now.day)), 'w')
 
 if __name__ == '__main__':
-    generateTask() # If run idenpendently, this script and be used to generate the task list
+    generateSeq144() # If run idenpendently, this script and be used to generate the task list
