d62bfbf8decca14f14757afe75203764cfc6ba42
Weichao Qiu <qiuwch@gmail.com>
2015-10-12 20:57:16 -0700

    Add script to generate lsp_2d_3d task.

---
+++ util/LSP3Dtask.py
@@ -0,0 +1,42 @@
+# Task should be able to serialize to files and read from file.
+# For each task should define the variables related to this task
+import sys
+sys.path.append('..')
+
+from datetime import datetime
+import copy
+from task import LSP3Dtask, Job
+
+
+
+def generate():
+    defTask = LSP3Dtask()
+    defTask.mode = 'ij'
+
+    tasks = []
+
+    # Fix cloth, random background, for each pose
+    for i in range(1, 1001):
+        t = copy.copy(defTask)
+        t.LSPPoseId = i
+        tasks.append(t)
+
+    j = Job()
+    j.name = 'LSP2D3D'
+    now = datetime.now(); j.date = '%02d.%02d' % (now.month, now.day)
+    j.comment = 'Use 2D joint annotation of LSP to manipulate human model'
+    j.outputFolder = '/q/cache/lsp_2d_3d/'
+    j.tasks = tasks
+    j.save('../scenes/%02d.%02d_lsp2to3.csv' % (now.month, now.day))
+
+if __name__ == '__main__':
+    generate() 
+
+
+
+
+
+# class MocapTask(Task):
+# 	def __init__(self):
+# 		Task.__init__(self)
+# 		self.PROP_LIST += ['backgroundId', 'frameId', 'clothId']
