73d8ef170efd674bf87f97d5b153f6f7426c2920
Weichao Qiu <qiuwch@gmail.com>
2015-10-14 22:21:21 -0700

    Update lsp_2d_3d task.

--- util/LSP3Dtask.py
+++ util/LSP3Dtask.py
@@ -11,12 +11,12 @@ from task import LSP3Dtask, Job
 
 def generate():
     defTask = LSP3Dtask()
-    defTask.mode = 'ij'
+    defTask.mode = 'ijd'
 
     tasks = []
 
     # Fix cloth, random background, for each pose
-    for i in range(1, 1001):
+    for i in range(1, 2001):
         t = copy.copy(defTask)
         t.LSPPoseId = i
         tasks.append(t)
@@ -25,7 +25,7 @@ def generate():
     j.name = 'LSP2D3D'
     now = datetime.now(); j.date = '%02d.%02d' % (now.month, now.day)
     j.comment = 'Use 2D joint annotation of LSP to manipulate human model'
-    j.outputFolder = '/q/cache/lsp_2d_3d/'
+    j.outputFolder = '/q/cache/lsp_2d_3d/render_output/'
     j.tasks = tasks
     j.save('../scenes/%02d.%02d_lsp2to3.csv' % (now.month, now.day))
 
