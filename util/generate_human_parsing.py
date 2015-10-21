4177e16286c789f90ecc0ba00b95f1cf06e08c04
Weichao Qiu <qiuwch@gmail.com>
2015-10-20 20:29:32 -0700

    Rename script for human parsing tasks.

--- util/generate_human_parsing.py
+++ util/generate_human_parsing.py
@@ -1,19 +1,29 @@
+import sys
+sys.path.append('../..')
+
 from datetime import datetime
 import random
+from tenon.task import HumanParsingTask, Job
+import tenon.cloth, tenon.background
 
-import sys
-sys.path.append('..')
-from task import Task, Job
-import config
 
 
 def generateSeq144():
     ''' Generate BVH Sequence 144_43, All types of clothes, Randomly pick a background '''
+    j = Job()
 
-    clothes = config.getClothList()
-    nCloth = len(clothes)
-    bgs = config.getBGList()
-    nBG = len(bgs)
+    j.clothFolder = '//textures/cloth/color/' # It seems blender is not good at handling . and ..
+    tenon.cloth.setClothFolder(j.clothFolder)
+    nCloth = len(tenon.cloth.clothTextures)
+
+    j.pantFolder = '//textures/cloth/color/' # It seems blender is not good at handling . and ..
+    tenon.cloth.setPantFolder(j.pantFolder)
+    nPant = len(tenon.cloth.pantTextures)
+
+    j.bgFolder = '//background/INRIA/'
+    tenon.background.setBGFolder(j.bgFolder)
+    nBG = len(tenon.background.bgFiles)
+
     print 'Number of clothes: %d, background: %d' % (nCloth, nBG)
 
     # Number of frames defined in the bvh file
@@ -25,20 +35,20 @@ def generateSeq144():
     tasks = []
 
     for iframe in range(nFrame):
-        task = Task()
+        task = HumanParsingTask()
         task.clothId = random.randrange(0, nCloth)
+        task.pantId = random.randrange(0, nPant)
         task.frameId = iframe
         task.backgroundId = random.randrange(0, nBG) # Pick from uniform
         task.mode = 'ipjd' # See definition
         tasks.append(task)
 
-    j = Job()
-    j.name = 'ClothParsing'
+    j.name = 'Human Parsing'
     j.date = '%02d.%02d' % (now.month, now.day)
     j.comment = 'This is the first batch task definition'
-    j.outputFolder = '/q/cache/clothParsing'
+    j.outputFolder = '/q/cache/human_parsing/'
     j.tasks = tasks
-    j.save(config.pathHelper('//%02d.%02d_Seq144.csv' % (now.month, now.day)))
+    j.save('../scenes/%02d.%02d_human_parsing.csv' % (now.month, now.day))
 
 if __name__ == '__main__':
     generateSeq144() # If run idenpendently, this script and be used to generate the task list
