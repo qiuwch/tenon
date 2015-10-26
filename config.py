d0798d426db108c70c39dc579408c184ea0b7fc8
Weichao Qiu <qiuwch@gmail.com>
2015-10-26 11:48:28 -0700

    Remove junk.

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
-import glob
 # Define global variables for this project.
-INRIA_DIR = '//background/INRIA/'
 RENDER_OUTPUT_DIR = '/q/cache/render_output/'
-TMP_DIR = '/q/cache/'
-JOINT_FILENAME = RENDER_OUTPUT_DIR + '/joint-PC.csv' # person centric annotation
 lspJointFile = '/q/cache/lsp_2d_3d/joint_3d/2015101415_v2/%04d.csv' # This is unnormalized version
-
-# human_model = 'human_model'
-# human_model = 'man_in_short'
 
 selectedBones = [
 	# 1. name of the bone
@@ -26,25 +19,6 @@ selectedBones = [
     ('upper_arm.fk.L', 'head'),
     ('upper_arm.fk.R', 'head'),
     ('neck', 'head')
-]
-
-# This is moved to puppet.py
-# Define the skeleton
-connections = [
-    # Leg
-    ('thigh.fk.L.head', 'thigh.fk.L.tail'),
-    ('thigh.fk.R.head', 'thigh.fk.R.tail'),
-    ('thigh.fk.L.tail', 'shin.fk.L.tail'),
-    ('thigh.fk.R.tail', 'shin.fk.R.tail'),
-    # Arm
-    ('upper_arm.fk.L.head', 'upper_arm.fk.L.tail'),
-    ('upper_arm.fk.R.head', 'upper_arm.fk.R.tail'),
-    ('upper_arm.fk.L.tail', 'forearm.fk.L.tail'),
-    ('upper_arm.fk.R.tail', 'forearm.fk.R.tail'),
-    # Head
-    ('head.tail', 'neck.head'),
-    ('thigh.fk.L.head', 'upper_arm.fk.L.head'),
-    ('thigh.fk.R.head', 'upper_arm.fk.R.head')
 ]
 
 def bpyPathHelper(relpath):
