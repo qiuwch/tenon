2f445e5466e33715a84d5a3f5973d6abd7a30e55
Weichao Qiu <qiuwch@gmail.com>
2015-10-26 19:21:38 -0700

    Rewrite cloth module, make it more robust and compact.

--- cloth.py
+++ cloth.py
@@ -1,58 +1,66 @@
 # Script to change clothes of makehuman model.
 import glob
+import logging
+import os
 from tenon.config import bpyPathHelper
 
-class ClothType:
-	Jeans, TShirt, LongTShirt, Shirt, Short = range(5)
+class Cloth:
+	textures = []
 
-textureConfig = {
-	ClothType.TShirt: 'tshirt02_texture.png',
-	ClothType.Jeans: 'jeans_basic_diffuse.png',
-	ClothType.Short: 'short01_black_diffuse.png'
-};
+	@classmethod
+	def setFolder(cls, folder):
+		folder = bpyPathHelper(folder)
+		jpgs = glob.glob('%s/*.jpg' % folder)
+		pngs = glob.glob('%s/*.png' % folder)
 
-clothTextures = []
-pantsTextures = []
+		cls.textures = jpgs + pngs
+		logging.info('Cloth folder is set to %s, num: %d' % (folder, len(cls.textures)))
 
-def setClothFolder(clothFolder):
-	global clothTextures
+	@classmethod
+	def changeByFilename(cls, filename):
+		if not os.path.isfile(filename):
+			logging.error('Background is set to a non-exsiting file %s' % filename)
 
-	clothFolder = bpyPathHelper(clothFolder)
-	jpgs = glob.glob('%s/*.jpg' % clothFolder)
-	pngs = glob.glob('%s/*.png' % clothFolder)
+		textureKey = cls.getTextureKey()
+		import bpy
+		textureImg = bpy.data.images[textureKey]
+		textureImg.filepath = bpy.path.relpath(filename)
 
-	clothTextures = jpgs + pngs
-	print('Cloth folder is set to %s, num: %d' % (clothFolder, len(clothTextures)))
+	@classmethod
+	def changeById(cls, id):
+		''' Change clothes by given id '''
+		if id < 0 or id > (len(cls.textures) - 1):
+			logging.error('Try changing pants to id %d, but index out of range' % id)
+		else:
+			filename = cls.textures[id]
+			cls.changeByFilename(filename)
 
-def setPantsFolder(pantsFolder):
-	global pantsTextures
+	@classmethod
+	def getTextureKey(cls):
+		import bpy
+		# Check which one exists
+		isValid = [bpy.data.images.get(k) != None for k in cls.textureKeys]
 
-	pantsFolder = bpyPathHelper(pantsFolder)
-	jpgs = glob.glob('%s/*.jpg' % pantsFolder)
-	pngs = glob.glob('%s/*.png' % pantsFolder)
-
-	pantsTextures = jpgs + pngs
-	print('Cloth folder is set to %s, num: %d' % (pantsFolder, len(pantsTextures)))
-
-
-def changeClothById(clothType, id):
-	global clothTextures
-	global pantsTextures
-	if clothType == ClothType.TShirt or clothType == ClothType.LongTShirt:
-		textures = clothTextures
-
-	if clothType == ClothType.Short or clothType == ClothType.Jeans:
-		textures = pantsTextures
-
-	''' Change clothes by given id '''
-	if id < 0 or id > (len(textures) - 1):
-		print('Index out of range')
-	else:
-		filename = textures[id]
-		changeClothByFilename(clothType, filename)
+		if not True in isValid:
+			logging.error('Can not find a suitable key for pants')
+			return None
+		else:
+			index = isValid.index(True)
+			return cls.textureKeys[index]
 
 
-def changeClothByFilename(clothType, filename):
-	import bpy
-	textureImg = bpy.data.images[textureConfig[clothType]]
-	textureImg.filepath = bpy.path.relpath(filename)
+
+class Pants(Cloth):
+	textureKeys = ['jeans_basic_diffuse.png', 'short01_black_diffuse.png', 'worksuit_diffuse.png']
+
+	@classmethod
+	def setTestingFolder(cls):
+		cls.setFolder('//textures/cloth/upper/')
+
+class TShirt(Cloth):
+	textureKeys = ['tshirt02_texture.png']
+
+	@classmethod
+	def setTestingFolder(cls):
+		cls.setFolder('//textures/cloth/lower/')
+
