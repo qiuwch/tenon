d7df26f1107a47fbe05a5b7808d7f2a86f4eb914
Weichao Qiu <qiuwch@gmail.com>
2015-10-28 19:53:56 -0700

    Update bpyPathHelper

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # Define global variables for this project.
 RENDER_OUTPUT_DIR = '/q/cache/render_output/'
-lspJointFile = '/q/cache/lsp_2d_3d/joint_3d/2015101415_v2/%04d.csv' # This is unnormalized version
 
 def bpyPathHelper(relpath):
     # Convert blender relpath to something accessible to other code
@@ -8,6 +7,10 @@ def bpyPathHelper(relpath):
         import bpy
         path = bpy.path.abspath(relpath)
     except:
-        path = relpath.replace('//', '../scenes/')
+        import os
+        folders = ['scenes', '../scenes'] # depends on where I execute the script
+        rootFolders = [v for v in folders if os.path.isdir(v)]
+
+        path = relpath.replace('//', rootFolders[0] + '/')
     return path
     
