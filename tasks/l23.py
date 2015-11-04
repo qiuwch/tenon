c0ee47c771dabb211513973c87121d5e4d9eae67
Weichao Qiu <qiuwch@gmail.com>
2015-11-04 10:29:40 -0800

    Update L23 task, move from csv to python.

--- tasks/l23.py
+++ tasks/l23.py
@@ -2,56 +2,30 @@ from tenon.tasks.basic import *
 from tenon.scene import Shirt, Pants, Bg, Lighting, Hair, Skin
 from tenon.render import Render
 
-def generate():
-    now = datetime.now();
-    jobInfo = ObjectDict({
-        'name': 'LSP2D3D',
-        'shirtFolder': '//textures/cloth/color/',
-        'pantsFolder': '//textures/cloth/lower/',
-        'filename': '//%s_%02d.%02d.csv' % (L23Job.__name__, now.month, now.day),
-        'bgFolder': '//background/INRIA/',
-        'outputFolder': '/q/cache/lsp_2d_3d/render_output/',
-        'comment': 'Use 2D joint annotation of LSP to manipulate human model',
-        'mode': 'ijd'
-    })
-
-    j = L23Job()
-    for k in jobInfo:
-        setattr(j, k, jobInfo[k])
-
-    defTask = L23Task() # Default task
-    defTask.mode = jobInfo.mode
-
-    tasks = []
-
-    Shirt.setFolder(j.shirtFolder)
-    nShirt = len(Shirt.textures)
-
-    Pants.setFolder(j.pantsFolder)
-    nShirt = len(Pants.textures)
-
-    Bg.setFolder(j.bgFolder)
-    nBG = len(Bg.textures)
-
-    for i in range(1, 2001):
-        t = copy.copy(defTask)
-        t.LSPPoseId = i
-        t.pantsId = random.randrange(0, nShirt)
-        t.bgId = random.randrange(0, nBG)
-        t.tshirtId = random.randrange(0, nShirt)
-        tasks.append(t)
-
-    j.tasks = tasks
-    j.save(bpyPathHelper(j.filename))
-
-
 class L23Job(Job):
-    def setupScene(self):
-        # This depends on the specific job requirement
-        # Meta information is only useful in this section
+    def config(s):
+        s.clothOptions = ['Texture', 'Color'] # Enum type
+        s.lightOptions = ['Environment', 'Random']
+
+        # Best option is Texture cloth + Random lighting + Random BG?
+        s.clothTexture = 'Texture'
+        s.clothTexture = 'Color'
+        s.randomBG = True
+
+
+
+    def __init__(self):
+        self.config()
+        self.outputFolder = '/q/cache/lsp_2d_3d/render_output/'
+        for i in range(1, 2001):
+            t = L23Task()
+            t.LSPPoseId = i
+            tasks.append(t)
+
+    def setupScene(self): # This will be executed before the job is run
         Shirt.setFolder('//textures/shirt')
         Pants.setFolder('//textures/pants')
-        Bg.setFolder(self.bgFolder)
+        Bg.setFolder('//background/INRIA')
         Hair.setFolder('//textures/hair')
         Skin.setFolder('//textures/skin')
 
@@ -65,7 +39,6 @@ class L23Job(Job):
         self.outputFolder = self._outputFolder + '/%s_%s/' % (sceneId, strTimeStamp()) # Put into a subfolder with timestamp
 
 
-
 class L23Task(Task):
     requiredProps = Task.requiredProps + ['LSPPoseId']
 
@@ -73,12 +46,6 @@ class L23Task(Task):
         Task.__init__(self)
         # Define the property list for this task
 
-    def setPose(self):
-        import tenon.puppet
-        logging.info('Animate to Pose %s' % self.LSPPoseId)
-        tenon.puppet.animateCP(int(self.LSPPoseId))
-        # tenon.pose.animateEditBone(int(self.LSPPoseId))
-        # This is for debugging
 
     def execute(self):
         self.prefix = 'im%04d' % (int(self.LSPPoseId)) # TODO: Let it start from 1
@@ -87,19 +54,20 @@ class L23Task(Task):
         #     logging.warning('%s exists, skip this task' % self.prefix)
         #     return
 
-        # TODO: timing this script to boost speed
-
-        logging.info('Change background to %s' % self.bgId)
-        Bg.changeById(self.bgId)
-
+        Bg.randomChange()
         Hair.randomChange()
         Skin.randomChange()
-        # Shirt.changeById(self.tshirtId)
-        # Pants.changeById(self.pantsId)
         Shirt.randomChange()
         Pants.randomChange()
         Lighting.setup()  # Randomly update the lighting
 
         self.setPose()
 
-        self.render()
+        self.render() # Define in Task
+
+    def setPose(self):
+        import tenon.puppet
+        logging.info('Animate to Pose %s' % self.LSPPoseId)
+        tenon.puppet.animateCP(int(self.LSPPoseId))
+        # tenon.pose.animateEditBone(int(self.LSPPoseId))
+        # This is for debugging
