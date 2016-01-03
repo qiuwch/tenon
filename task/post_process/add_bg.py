0ef02e5a89edc38cd788743c268bd67aee8997d4
Weichao Qiu <qiuwch@gmail.com>
2016-01-03 00:19:03 -0800

    Update add_bg script. Add command options.

--- task/post_process/add_bg.py
+++ task/post_process/add_bg.py
@@ -1,26 +1,59 @@
+import sys
+sys.path.append('../../tenon/')
+import util
+
 import glob
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
+import random
+import skimage.io
+import numpy as np
+import argparse
 
-nobgfiles = glob.glob('/q/cache/tenon/rendered/no_bg/imgs/*.png')
+def composite(nobgfolder):
+    nobgfolderCrop = os.path.join(nobgfolder, 'crop')
+    compositefolder = os.path.join(nobgfolderCrop, 'composite')
+    # nobgfolder = '/q/cache/tenon/rendered/no_bg_1128/crop/'
+    # compositefolder = '/q/cache/tenon/rendered/no_bg_1128/crop/composite/'
 
-for i in nobgfiles:
-    print(i)
-    im = plt.imread(i)
-    plt.imshow(im)
+    if not os.path.isdir(compositefolder):
+        os.mkdir(compositefolder)
+        
+    nobgfiles = glob.glob(os.path.join(nobgfolderCrop, 'imgs/*.png'))
+    bgfolder = '/q/data/INRIA/'
+    bgfiles = glob.glob(bgfolder + '*.jpg')
 
-    # Load joint location
-    csvfile = '/q/cache/tenon/rendered/no_bg/joint/im0001.csv'
-    df = pd.read_csv(csvfile, header=None)
+    pg = util.ProgressBar(len(nobgfiles))
+    for i in range(len(nobgfiles)):
+        pg.update(i)
+        nobgfile = nobgfiles[i]
+        im = skimage.io.imread(nobgfile)
+        alpha = im[:,:,3]
+        im = im[:,:,0:3]    
+        [h, w, c] = im.shape
+        
+        bgid = random.randint(0, len(bgfiles)-1)
+        bgfile = bgfiles[bgid]
+        # bg = plt.imread(bgfile)
+        bg = skimage.io.imread(bgfile)
+        assert(im.dtype == bg.dtype)
+        bgCrop = bg[0:h, 0:w, :]
 
-    def cropPatch():
-        for i in len(df[0]):
-            jointName = df[0][i]
-            x = df[1][i]
-            y = df[2][i]
+        fgmask = np.tile(alpha[:,:,np.newaxis] / 255.0, (1, 1, 3));
+        bgmask = 1 - fgmask;
+        combine = bgCrop * bgmask + im * fgmask
+        combine = combine.astype('uint8')
+        
+        combinefile = os.path.join(compositefolder, os.path.basename(nobgfile))
+        skimage.io.imsave(combinefile, combine)
 
-            plt.plot(x, y)
+def main():
+    parser = argparse.ArgumentParser(description = 'Add random background to no bg images')
+    parser.add_argument('nobgfolder', help='Image without bg')
 
-    plt.savefig('%s.png' % os.path.basename(i))
+    args = parser.parse_args()
+    composite(args.nobgfolder)
 
+if __name__ == '__main__':
+    main()
