e46d72f1622d7ccaa1639140988880aa21c434df
Weichao Qiu <qiuwch@gmail.com>
2016-01-04 21:29:25 -0800

    Minor update.

--- task/render/render_texture.py
+++ task/render/render_texture.py
@@ -1,6 +1,6 @@
 import sys 
 sys.path.append('.')
-sys.path.append('..')
+sys.path.append('../..')
 import tenon
 import tenon.util
 import os
@@ -9,13 +9,16 @@ from l23 import L23Job
 
 def main():
     timestamp = tenon.util.shorttimestamp()
-    logfile = '%s/render_no_bg_%s.log' % (os.getcwd(), timestamp)
+    logfile = '%s/render_texture_%s.log' % (os.getcwd(), timestamp)
     tenon.util.startlogging(logfile)
 
-    l23_texture_cloth = L23Job('l23textureBrodatz')
-    l23_texture_cloth.cloth = 'textureBrodatz'
+    l23_texture_cloth = L23Job()
+    l23_texture_cloth.shirtFolder = '//textures/ColorBrodatzPng'
+    l23_texture_cloth.pantsFolder = '//textures/ColorBrodatzPng'
+    l23_texture_cloth.skinFolder = '//textures/randSkinColor'
+
     l23_texture_cloth.comment = 'L23 dataset with Texture pattern as cloth'
-    l23_texture_cloth.outputFolder = './texture/'
+    l23_texture_cloth.outputFolder = './textureCloth/'
     l23_texture_cloth.run()
 
 if __name__ == '__main__':
